c0c857d6339fd8d97b1c75e596f940deb911f197
shushnikz <sushmithadevadiga997@gmail.com>
2023-05-21 19:38:20 +0530

    added comments

--- src/Addtwonumbers.js
+++ src/Addtwonumbers.js
@@ -4,14 +4,23 @@ import axios from "axios";
 
 function Addtwonumbers() {
 
+    // createing two state to store two numbers
+
     const [number1, setNumber1] = useState('')
     const [number2, setNumber2] = useState('')
+
+    // creating state to store resultant number
+
     const [addNumber, setAddNumber] = useState('')
 
+    // handleadd function to fetch the sum whenc click on add
     const handleAdd = async (e) => {
         e.preventDefault()
 
         try {
+
+            // fetch api using axios
+
             const result = await axios.post('https://pre-flight-backend.onrender.com/add', { number1, number2 })
             console.log(result)
             setAddNumber(result.data.result);
@@ -24,6 +33,7 @@ function Addtwonumbers() {
         <div className='main'>
             <h1>Add Two Numbers</h1>
             <div className='input'>
+                {/* Input box to enter number */}
                 <Input type='number'
                     value={number1}
                     onChange={(e) => {
@@ -39,9 +49,11 @@ function Addtwonumbers() {
                     }}
                     placeholder='Enter number 2' />
 
+                {/* Button to submit the value */}
                 <Button onClick={handleAdd}>Add Number</Button>
             </div>
             <div>
+                {/* Display Summ of two numbers */}
                 <h3>{addNumber && <p>Result: {addNumber}</p>}</h3>
             </div>
         </div>
